8d18d4206d47950b47fd12c80b8b767c29770130
Han-16 <haanbk16@gmail.com>
2023-04-04 15:43:49 +0900

    question_create complete

--- pybo/views.py
+++ pybo/views.py
@@ -23,6 +23,14 @@ def answer_create(request, question_id):
     return redirect('pybo:detail', question_id = question.id)
 
 def question_create(request):
-    form = QuestionForm()
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('pybo:index')
+    else:
+        form = QuestionForm()
     return render(request, 'pybo/question_form.html',
                   {'form' : form})
