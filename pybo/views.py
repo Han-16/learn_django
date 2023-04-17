7aeada137b065901492740bdfc0370ffd1b9fe65
Han-16 <haanbk16@gmail.com>
2023-04-17 23:06:56 +0900

    3-07 모델 변경

--- pybo/views.py
+++ pybo/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponseNotAllowed
+# from django.http import HttpResponseNotAllowed
 from .models import Question
 from django.utils import timezone
 from .forms import QuestionForm, AnswerForm
 from django.core.paginator import Paginator
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def index(request):
@@ -21,29 +21,35 @@ def detail(request, question_id):
     context = { 'question' : question } # key값이 template에서 사용할 변수이름, value 값이 파이썬 변수
     return render(request, "pybo/question_detail.html", context)
 
+
+@login_required(login_url='common:login')
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk = question_id)
     if request.method == "POST":
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit=False)
+            answer.author = request.user
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
             return redirect('pybo:detail', question_id=question.id)
     else:
-        return HttpResponseNotAllowed('Only POST is possible.')
+        form = AnswerForm()
     context = {'question': question, 'form': form}
     return render(request, 'pybo/question_detail.html', context)
 
     # question.answer_set.create(content=request.POST.get('content'), create_date = timezone.now())
     # return redirect('pybo:detail', question_id = question.id)
 
+
+@login_required(login_url='common:login')
 def question_create(request):
     if request.method == 'POST':
         form = QuestionForm(request.POST)
         if form.is_valid():
             question = form.save(commit=False)     # QuestionForm은 question 데이터베이스와 연결되어있음
+            question.author = request.user
             question.create_date = timezone.now()  # 그래서 바로 저장을 하게되면 create_date가 없어서 오류가 남.
             question.save()
             return redirect('pybo:index')
