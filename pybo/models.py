432821363a82ae5425d314b2047a9d166a4e002c
Han-16 <haanbk16@gmail.com>
2023-05-26 20:14:41 +0900

    질문/답변에 댓글 기능 추가

--- pybo/models.py
+++ pybo/models.py
@@ -27,6 +27,6 @@ class Comment(models.Model):
     author = models.ForeignKey(User, on_delete = models.CASCADE)
     content = models.TextField()
     create_date = models.DateTimeField()
-    modify_date = models.DateTimeField(null=True, blank = True)
+    modify_date = models.DateTimeField(null = True, blank = True)
     question = models.ForeignKey(Question, null = True, blank = True, on_delete = models.CASCADE)
-    answer = models.ForeignKey(Answer, null= True, blank = True, on_delete = models.CASCADE)
+    answer = models.ForeignKey(Answer, null = True, blank = True, on_delete = models.CASCADE)
